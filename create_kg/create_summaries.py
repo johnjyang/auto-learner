2993c3af1b043818f845fb86f9619582e2c12863
John Yang <75066915+johnjyang@users.noreply.github.com>
2022-01-10 10:55:48 -0800

    fix: create_summaries filter

--- create_kg/create_summaries.py
+++ create_kg/create_summaries.py
@@ -24,11 +24,13 @@ for c in range(len(clusters)):
                               max_length=15,
                               min_length=1,
                               do_sample=False)[0]['summary_text']
-    summary_text = summary_text[1:].split(',')[0]
-    summary_text = summary_text.split(':')[0]
+    summary_text = summary_text.replace('"', '')
+    summary_text = summary_text.split(',')[0]
+    summary_text = summary_text.split(' . ')[0]
+    summary_text = summary_text.split(':  ')[0]
     summary_text = summary_text.split('is')[0]
-    summary_text = summary_text.strip()
-    summaries.append(summary_text)
+    summary_text = summary_text.lstrip().strip()
+    summaries.append(summary_text.lower())
     print("Summarized cluster " + str(c + 1) + "/" + str(len(clusters)))
 print("Summarizing completed in {:.2f} secs.".format(time.time() - start_time))
 
