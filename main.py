619429464b4c9949e96a7923122df9d8842b1711
John Yang <75066915+johnjyang@users.noreply.github.com>
2022-01-12 08:16:55 -0800

    run main

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ from create_kg.create_summaries import *
 
 # clean html and log searches
 load_dotenv()
-# cleaned_html = init_clean(os.getenv('HTML_FILE_NAME'))
-# log_searches(cleaned_html)
+cleaned_html = init_clean(os.getenv('HTML_FILE_NAME'))
+log_searches(cleaned_html)
 
 # create and log clusters_0 from searches
-# searches = get_data('searches')
+searches = get_data('searches')
 cluster_model = SentenceTransformer('all-mpnet-base-v2')
-# clusters_0_raw = create_clusters(searches, cluster_model, 0.6)
-# log_clusters('clusters_0', clusters_0_raw, searches)
-# log_clusters('clusters_0_repeat', clusters_0_raw, searches, True)
+clusters_0_raw = create_clusters(searches, cluster_model, 0.6)
+log_clusters('clusters_0', clusters_0_raw, searches)
+log_clusters('clusters_0_repeat', clusters_0_raw, searches, True)
 
 
 # create and log summaries_0 from clusters_0
-# clusters_0 = get_data('clusters_0')
+clusters_0 = get_data('clusters_0')
 os.environ['CUDA_VISIBLE_DEVICES'] = os.getenv('CUDA_GPU_COUNT')
 summarize_model = pipeline('summarization', model='sshleifer/distilbart-cnn-12-6')
-# summaries_0_raw = summarize_clusters(clusters_0, summarize_model)
-# log_summaries('summaries_0', summaries_0_raw)
+summaries_0_raw = summarize_clusters(clusters_0, summarize_model)
+log_summaries('summaries_0', summaries_0_raw)
 
 # create and log clusters_1 from summaries_0
 summaries_0 = get_data('summaries_0')
