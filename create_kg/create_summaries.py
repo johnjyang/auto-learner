9d83a7dd1bb4ca38a3bb219c2ed9fe3bb07507a5
John Yang <75066915+johnjyang@users.noreply.github.com>
2022-01-12 07:56:07 -0800

    fix: create_summaries filter

--- create_kg/create_summaries.py
+++ create_kg/create_summaries.py
@@ -11,20 +11,35 @@ def summarize_clusters(clusters, model):
     start_time = time.time()
     summaries = []
     for c in range(len(clusters)):
-        summary_text = model(clusters[c],
-                                max_length=15,
-                                min_length=1,
-                                do_sample=False)[0]['summary_text']
-        summary_text = summary_text.replace('"', '')
-        summary_text = summary_text.split(',')[0]
-        summary_text = summary_text.split(' .')[0]
-        summary_text = summary_text.split(':  ')[0]
-        summary_text = summary_text.split(' is ')[0]
-        summary_text = summary_text.split(' are ')[0]
-        summary_text = summary_text.lstrip().strip()
-        if summary_text:
-            summaries.append(summary_text.lower())
-        print("Summarized cluster " + str(c + 1) + "/" + str(len(clusters)))
+        if not clusters[c].split(',')[0].split(' ')[0]:
+            summary_text = clusters[c].split(' ')[1]
+        else:
+            summary_text = model(clusters[c],
+                                    max_length=15,
+                                    min_length=1,
+                                    do_sample=False)[0]['summary_text']
+            summary_text = summary_text.lower().lstrip()
+            summary_text = summary_text.replace('"', '')
+            summary_text = summary_text.split(',')[0]
+            summary_text = summary_text.split(' .')[0]
+            summary_text = summary_text.split(':  ')[0]
+            if not summary_text in clusters[c]:
+                if summary_text[:2] != 'is' and summary_text[:3] != 'are' and summary_text[:7] != 'include':
+                    summary_text = summary_text.split(' is ')[0]
+                    summary_text = summary_text.split(' are ')[0]
+                    summary_text = summary_text.split(' include ')[0]
+                    summary_text = summary_text.split('?')[0]
+            summary_text = summary_text.lstrip().strip()
+            if summary_text:
+                summaries.append(summary_text)
+            else:
+                summaries.append(clusters[c].split(',')[0])
+                print(str(f'Error: cluster {c}, {clusters[c]}, {summary_text}'))
+                write_txt.list_of_str_to_txt(
+                    'errors', [str(f'Error: cluster {c}, {clusters[c]}, {summary_text}')])
+        print(
+            f'Summarized cluster {str(c + 1)}/{str(len(clusters))}: {summary_text.lower()}'
+        )
     print("Summarizing completed in {:.2f} secs.".format(time.time() - start_time))
     return summaries
 
